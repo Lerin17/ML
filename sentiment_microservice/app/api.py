891d1fddc11efb4f7eb42c9093cc36f4425ab6fe
Lerin17 <lerinowoade17@gmail.com>
2025-05-01 08:01:18 -0700

    #1 initial setup👌

---
+++ sentiment_microservice/app/api.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from app.schema import TextInput, SentimentOutput
+from app.service import analyze_sentiment
+
+router = APIRouter(prefix="/sentiment", tags=["sentiment"])
+
+@router.post("/analyze", response_model=SentimentOutput)
+def analyze(input: TextInput):
+    sentiment = analyze_sentiment(input.text)
+    return SentimentOutput(sentiment=sentiment)
